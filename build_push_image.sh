70894d4fccd1dcbc1d92554a7c891057aa6cdaeb
dannyferdiansyah <dannyferdiansyah389@gmail.com>
2023-09-06 18:24:56 +0700

    Commit

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,9 +2,10 @@
 
 IMAGE_NAME="item-app"
 IMAGE_TAG="v1"
-DOCKER_IMAGE_NAME="dannyferdiansyah/$IMAGE_NAME"
+DOCKERHUB_IMAGE_NAME="dannyferdiansyah/$IMAGE_NAME"
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
 docker images
-docker tag $IMAGE_NAME:$IMAGE_TAG $DOCKER_IMAGE_NAME:$IMAGE_TAG
+docker tag $IMAGE_NAME:$IMAGE_TAG $DOCKERHUB_IMAGE_NAME:$IMAGE_TAG
 docker login
-docker push $DOCKER_IMAGE_NAME:$IMAGE_TAG
+docker push $DOCKERHUB_IMAGE_NAME:$IMAGE_TAG
+echo "Image berhasil diunggah ke Docker Hub dengan nama: $DOCKERHUB_IMAGE_NAME:$IMAGE_TAG"
